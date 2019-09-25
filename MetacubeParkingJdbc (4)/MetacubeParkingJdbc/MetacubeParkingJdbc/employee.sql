d2607e7279f017d2620a1b6e9389abaa5adff55a
Khushi-Ag123456 <55191949+Khushi-Ag123456@users.noreply.github.com>
2019-09-25 08:25:28 +0530

    Add files via upload

---
+++ MetacubeParkingJdbc (4)/MetacubeParkingJdbc/MetacubeParkingJdbc/employee.sql
@@ -0,0 +1,52 @@
+Drop DATABASE IF EXISTS Employee;
+
+CREATE DATABASE Employee;
+USE Employee;
+
+CREATE TABLE employeeDetails (
+empId int NOT NULL AUTO_INCREMENT,
+fullName varchar(30),
+gender varchar(30) CHECK(gender IN('Male', 'Female', 'Other')),
+emailId varchar(30) UNIQUE,
+password varchar(30),
+contactNo varchar(15),
+organization varchar(30),
+image varchar(200),
+PRIMARY KEY(empId)
+);
+
+SELECT LAST_INSERT_ID() from employeeDetails;
+
+select * from employeeDetails;
+ 
+CREATE TABLE Vehicle (
+empId int not null,
+name varchar(30),
+type varchar(30),
+vehicle_number varchar(30),
+identification varchar(100),
+foreign key (empId) references employeeDetails(empId)
+);
+
+alter table Vehicle
+Add index vehicleType(type);
+
+select * from Vehicle;
+
+create table pass (
+vehicle_type varchar(20),
+daily double,
+monthly double,
+yearly double
+);
+
+
+alter table employeeDetails
+add imageUrl BLOB;
+
+Insert into pass values('Cycle', 0.070, 1.39, 6.97),
+('Bike', 0.14, 2.79, 13.95),
+('Four_Wheeler', 0.28, 6.97, 48.81);
+
+
+select * from employeeDetails where emailId = 'khushi@gmail.com';
